58ca2149b937b4025d762e768078860fe7e0ce2a
Agnieszka <agnesliszk@gmail.com>
2021-05-13 20:16:22 +0200

    Add validation

--- src/components/MedicineCart.vue
+++ src/components/MedicineCart.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="medicineCart">
-    <el-form label-position="top" :inline="true" class="form">
-      <el-form-item>
+    <el-form
+      label-position="top"
+      :model="ruleForm"
+      :rules="formRules"
+      :inline="true"
+      class="form"
+      ref="ruleForm"
+    >
+      <el-form-item prop="medicineName">
         <el-input
-          v-model="medicineNameInput"
+          v-model="ruleForm.medicineName"
           placeholder="Input a medicine"
         ></el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop="expiryDate">
         <div class="block">
           <el-date-picker
-            v-model="expiryDate"
+            v-model="ruleForm.expiryDate"
             type="date"
             placeholder="Pick a day"
           >
@@ -18,7 +25,7 @@
         </div>
       </el-form-item>
       <el-form-item class="button">
-        <el-button type="warning" @click="addMedicine"
+        <el-button type="warning" @click="addMedicine()"
           >Add a medicine</el-button
         >
       </el-form-item>
@@ -33,13 +40,38 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 export default class MedicineCart extends Vue {
   // @Prop() private msg!: string;
 
-  medicineNameInput = "";
-  expiryDate = "";
+  ruleForm: { medicineName: string; expiryDate: string } = {
+    medicineName: "",
+    expiryDate: "",
+  };
+
   medicineList: {
-    name: string;
-    date: string;
+    medicineName: string;
+    expiryDate: string;
     expired: boolean;
   }[] = [];
+
+  formRules = {
+    medicineName: [
+      {
+        required: true,
+        message: "Please input medicine",
+        trigger: "blur",
+      },
+      {
+        min: 3,
+        message: "Minimal length should be 3 letters",
+        trigger: "blur",
+      },
+    ],
+    expiryDate: [
+      {
+        required: true,
+        message: "Please input expiry date",
+        trigger: "blur",
+      },
+    ],
+  };
 
   isMedicineExpired(expiryDate: string) {
     let today = new Date().setHours(0, 0, 0, 0);
@@ -52,14 +84,19 @@ export default class MedicineCart extends Vue {
   }
 
   addMedicine() {
-    this.medicineList.push({
-      name: this.medicineNameInput,
-      date: this.expiryDate,
-      expired: this.isMedicineExpired(this.expiryDate),
-    });
-
-    console.log("submit!");
-    console.log(this.medicineList);
+    let formName: any = this.$refs.ruleForm;
+    if (formName.validate) {
+      this.medicineList.push({
+        medicineName: this.ruleForm.medicineName,
+        expiryDate: this.ruleForm.expiryDate,
+        expired: this.isMedicineExpired(this.ruleForm.expiryDate),
+      });
+      console.log("submit!");
+      console.log(this.medicineList);
+    } else {
+      console.log("error submit!!");
+      return false;
+    }
   }
 }
 </script>
